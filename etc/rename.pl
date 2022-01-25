8a1e2d7969b43543d83ae2a46526565ede09be49
Georg Fischer <dr.Georg.Fischer@gmail.com>
2022-01-25 18:16:37 +0100

    rename.pl for log4j

--- etc/rename.pl
+++ etc/rename.pl
@@ -2,23 +2,9 @@
 
 # rename some variables
 # @(#) $Id: rename.pl 113 2009-04-06 14:57:07Z gfis $
+# 2022-01-25: for the log4j 1.x -> 2.17.1 conversion
 # 2009-04-06: Dr. Georg Fischer
 #-------------------------------------------------------
-#
-#  Copyright 2006 Dr. Georg Fischer <punctum at punctum dot kom>
-# 
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-# 
-#       http://www.apache.org/licenses/LICENSE-2.0
-# 
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
 
 use strict;
 use utf8;
